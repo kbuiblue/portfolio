8e3e7d0b7591fbd3bc8175c8fc710096cc718659
Khanh Bui <kbuiblue@gmail.com>
2024-05-08 23:40:36 +0700

    chore: additional updates and setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), mdx()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "nord",
-      wrap: false,
-    },
-  },
+      wrap: false
+    }
+  }
 });
