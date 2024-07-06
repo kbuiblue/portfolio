3da9213f9557ebe2f749fdf61408b946f2e98f3c
Khanh Bui <kbuiblue@gmail.com>
2024-07-06 14:02:36 +0700

    chore: add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
-
 import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap(), mdx(), icon()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
