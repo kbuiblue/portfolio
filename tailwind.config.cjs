f05274b1561dd1d43662177548a45140a27865b0
Khanh Bui <kbuiblue@gmail.com>
2024-07-06 14:06:23 +0700

    feat: add custom heart-pulse animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,23 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Jetbrains Mono", ...defaultTheme.fontFamily.sans],
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Jetbrains Mono", ...defaultTheme.fontFamily.sans],
+            },
+            animation: {
+                'heart-pulse': 'heartPulse 1s ease-in-out infinite',
+            },
+            keyframes: {
+                heartPulse: {
+                    "0%": { transform: "scale(1)" },
+                    "75%": { transform: "scale(1.3)" },
+                    "100%": { transform: "scale(1)" },
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
