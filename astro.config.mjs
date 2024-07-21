8998d10333ae0f737b153eb6371ac1cd90487f5e
Khanh Bui <kbuiblue@gmail.com>
2024-07-21 14:26:54 +0700

    chore: remove config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from "./src/data/config";
 import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
@@ -9,7 +8,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), mdx(), icon()],
-  site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
