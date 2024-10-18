388e8d54949b9dc0414d4415bbe68754f5e2e828
Khanh Bui <kbuiblue@gmail.com>
2024-10-18 17:04:28 +0700

    reset: add Accolades component

---
+++ src/data/accolades.ts
@@ -0,0 +1,15 @@
+export type Accolade = {
+    linkClass: string;
+    href: string;
+    imageClass: string;
+    alt: string;
+};
+
+export const accolades: Accolade[] = [
+    {
+        linkClass: "hover:border-orange-500",
+        href: "https://uk.trustpilot.com/reviews/65b0bd6949ef9d520408441d",
+        imageClass: "rounded-md hover:border-orange-500",
+        alt: "Volcanic Beginner Accolade",
+    },
+];
